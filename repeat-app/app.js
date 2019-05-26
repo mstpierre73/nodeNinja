303fc929f5f8a751acf4ab66cc059e620fbfde8a
mstpierre73 <naranjaqc@hotmail.com>
2019-05-26 08:00:49 -0400

    add express to the project

--- repeat-app/app.js
+++ repeat-app/app.js
@@ -7,15 +7,24 @@ app.get("/", function(req, res){
 });
 
 //Defining routes for animals
-
+app.get("/animals", function(req, res){
+    let animals = req.params.animals;
+    res.send("Send the appropriate noise following the animal");
+})
 
 //Defining routes for repeat
-
+app.get("/repeat/word/number", function(req, res){
+    let word = req.params.word;
+    let number = req.params.number;
+    res.send("Send the appropriate word, the number of times required");
+})
 
 //Defining error 404 route
-
+app.get("*", function(req, res){
+    res.send("Serving the error 404 page");
+})
 
 //Listening for the server
 app.listen(3000, function(){
-    console.log("The server is listening on port 3000");
+    console.log("My own server is listening on port 3000");
 });
